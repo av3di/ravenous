8edc4739f830f61e29bad008cf0afd59bc0cff0d
av3di <e_uribe2011@yahoo.com>
2024-09-13 12:07:50 -0700

    convert business to obj literal

--- src/components/Business.js
+++ src/components/Business.js
@@ -3,20 +3,31 @@ import './Business.css';
 import logo from '../logo.svg';
 
 function Business() {
+  const business1 = {
+    imageSrc: logo,
+    name: 'Pizza Hut',
+    address: '123 Wysteria Lane',
+    city: 'Bordertown',
+    state: 'MI',
+    zipCode: '10100',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90,
+  };
   return (
     <div className="business">
-      <img src={logo} />
-      <h4>Pizza Hut</h4>
+      <img src={business1.imageSrc} />
+      <h4>{business1.name}</h4>
       <div className="business-info">
         <div className="text-left">
-          <p>123 Wysteria Lane</p>
-          <p>Bordertown</p>
-          <p>MI 1010</p>
+          <p>{business1.address}</p>
+          <p>{business1.city}</p>
+          <p>{business1.state} {business1.zipCode}</p>
         </div>
         <div className="text-right">
-          <p>Italian</p>
-          <p>4.5 stars</p>
-          <p>1000 reviews</p>
+          <p>{business1.category}</p>
+          <p>{business1.rating} stars</p>
+          <p>{business1.reviewCount} reviews</p>
         </div>
       </div>
     </div>
