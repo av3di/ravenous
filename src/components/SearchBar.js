0b1d1ac5df665d308467107241accb0f08547a0b
av3di <e_uribe2011@yahoo.com>
2024-09-17 13:18:41 -0700

    add sort label, add state hook for searchbar inputs

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
+  const [sort, setSort] = useState('best_match');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleSearchTermChange = ({target}) => setSearchTerm(target.value);
+  
+  const handleLocationChange = ({target}) => setLocation(target.value);
+
   return (
     <div>
       <div class="first-row">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input value={searchTerm} onChange={handleSearchTermChange} placeholder="Search Businesses" />
+        <input value={location} onChange={handleLocationChange} placeholder="Where?" />
       </div>
       <div class="second-row">
-        <select>
-          <option>Best Match</option>
-          <option>Highest Rated</option>
-          <option>Most Reviewed</option>
+        <label for="sort_by">Sort by:</label>
+        <select id="sort_by">
+          <option value="best_match">Best Match</option>
+          <option value="rating">Highest Rated</option>
+          <option value="review_count">Most Reviewed</option>
         </select>
         <button>Search</button>
       </div>
